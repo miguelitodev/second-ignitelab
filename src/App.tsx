aefc6b22e69cc683974085c0a8be242605c4244f
miguelrisquelme <miguelrisquelme@gmail.com>
2022-06-22 11:02:25 -0300

    Adding the sidebar component and others structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-	query {
-		lessons {
-			id
-			title
-		}
-	}
-`;
-
-interface Lessons {
-	id: string;
-	title: string;
-}
+import { Event } from "./pages/Event";
 
 function App() {
-	const { data } = useQuery<{ lessons: Lessons[] }>(GET_LESSONS_QUERY);
-	console.log(data);
-
-	return <h1 className="text-2xl">Hello</h1>;
+	return <Event />;
 }
 
 export default App;
